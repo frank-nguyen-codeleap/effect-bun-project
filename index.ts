6392b8e3bfeb6d0bff5474b8558284147ffe20d0
Frank Nguyen <frank.nguyen@codeleap.de>
2024-10-25 16:24:02 +0700

    implement Ref data type to store partial result

--- index.ts
+++ index.ts
@@ -1,34 +1,33 @@
-import { Effect, Fiber } from "effect";
+import { Effect, Fiber, Ref } from "effect";
 import { getPokemon } from "./api/pokemon.api.js";
 import type { Pokemon } from "./models/pokemon.model.js";
-import type { JsonParseError, NetworkError } from "./errors.js";
 import type { ParseError } from "@effect/schema/ParseResult";
+import type { JsonParseError, NetworkError } from "./errors.js";
 
 const taskDurationInMs = 300;
 const pokemons = ["totodile", "snorlax", "mew", "abra"];
 
 /**
- * Long running fetch-pokemon task that can be interrupted by another Fiber.
- * @returns {Effect<Pokemon[], ParseError | NetworkError | JsonParseError>} The effect that fetches the information.
+ * Long running get-pokemon task that can be interrupted by another Fiber.
+ * @returns {Effect<void, ParseError | NetworkError | JsonParseError>} The effect that fetches the information.
  */
-const getPokemonProgram: Effect.Effect<
-  Pokemon[],
-  ParseError | NetworkError | JsonParseError,
-  never
-> = Effect.gen(function* () {
-  const results = [];
-  for (const pokemon of pokemons) {
-    yield* Effect.log(`Fetching ${pokemon}...`);
-    results.push(yield* getPokemon(pokemon));
-  }
-  return results;
-});
+const getPokemonProgram = (
+  ref: Ref.Ref<Array<Pokemon>>
+): Effect.Effect<void, ParseError | NetworkError | JsonParseError> =>
+  Effect.gen(function* () {
+    for (const pokemon of pokemons) {
+      const pkm = yield* getPokemon(pokemon);
+      yield* Effect.log(`Fetched ${pokemon}...`);
+      yield* Ref.update(ref, (curRes) => [...curRes, pkm]);
+    }
+  });
 
 /**
  * Represents the main program that executes a series of effects.
  */
 const program = Effect.gen(function* () {
-  const getPokemonFiber = yield* Effect.fork(getPokemonProgram);
+  const resultRef = yield* Ref.make<Array<Pokemon>>([]);
+  const getPokemonFiber = yield* Effect.fork(getPokemonProgram(resultRef));
 
   yield* Effect.sleep(taskDurationInMs);
   yield* Effect.log(
@@ -37,8 +36,12 @@ const program = Effect.gen(function* () {
   // Interrupt the currently running task
   yield* Fiber.interrupt(getPokemonFiber);
 
-  const result = yield* Effect.either(Fiber.join(getPokemonFiber));
-  yield* Effect.log(`Result: ${result}`);
+  // Read results from Ref
+  const partialRes = yield* Ref.get(resultRef);
+  yield* Effect.log(`Partial result: ${JSON.stringify(partialRes, null, 2)}`);
+
+  const finalRes = yield* Effect.either(Fiber.join(getPokemonFiber));
+  yield* Effect.log(`Final result: ${finalRes}`);
 });
 
 Effect.runPromise(program).then(console.log).catch(console.error);
