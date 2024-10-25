1c473da9648077f97689e284f1359e0ccde82f50
Frank Nguyen <frank.nguyen@codeleap.de>
2024-10-25 14:14:26 +0700

    implement fiber interruption

--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
-import { Effect } from "effect";
+import { Effect, Fiber } from "effect";
 import { getPokemon } from "./api/pokemon.api.js";
 
+const taskDurationInMs = 300;
 const pokemons = ["totodile", "snorlax", "mew", "abra"];
-const program = Effect.gen(function* () {
+
+const getPokemonProgram = Effect.gen(function* () {
   const results = [];
   for (const pokemon of pokemons) {
+    yield* Effect.log(`Fetching ${pokemon}...`);
     const result = yield* getPokemon(pokemon);
     results.push(result);
   }
   return results;
 });
 
-Effect.runPromise(program).then(console.log);
+const program = Effect.gen(function* () {
+  const getPokemonFiber = yield* Effect.fork(getPokemonProgram);
+
+  yield* Effect.sleep(taskDurationInMs);
+  yield* Effect.log(`${taskDurationInMs}ms have passed! Interrupting getPokemon task!`);
+  yield* Fiber.interrupt(getPokemonFiber);
+
+  const result = yield* Effect.either(Fiber.join(getPokemonFiber));
+  yield* Effect.log(`Result: ${result}`);
+});
+
+Effect.runPromise(program).then(console.log).catch(console.error);
